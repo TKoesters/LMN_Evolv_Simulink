c42a8c064b8be484a472067cb1202819195687ae
TKoesters <tarek.koesters@uni-siegen.de>
2023-04-20 16:41:13 +0200

    changes for code generation

--- @LMN_evolv/getMaxDelay.m
+++ @LMN_evolv/getMaxDelay.m
@@ -2,6 +2,24 @@ function [maxDelay,index] = getMaxDelay(obj)
 %GETMAXDELAY Summary of this function goes here
 %   Detailed explanation goes here
 
-[maxDelay,index] = max([cell2mat(obj.xDynInputDelay),cell2mat(obj.xDynOutputDelay)]);
+%% code generation
+maxDelay = 0;
+index = 1;
+for i = 1 : obj.dimIn
+    if obj.xDynInputDelay{i}>maxDelay
+        index = i;
+        maxDelay = obj.xDynInputDelay{i};
+    end
+end
+
+if ~isempty(obj.xDynOutputDelay)
+    if obj.xDynOutputDelay{1} > maxDelay
+        index = i + 1;
+        maxDelay = obj.xDynOutputDelay{1};
+    end
+end
+
+%% cell2mat
+%[maxDelay,index] = max([cell2mat(obj.xDynInputDelay),cell2mat(obj.xDynOutputDelay)]);
 
 end

0574596061a01c16a5028b534145fad1527d359c
TKoesters <tarek.koesters@uni-siegen.de>
2023-07-13 16:57:58 +0200

    handling of inputs in reNormInputs

--- @LMN_evolv/reNormInputs.m
+++ @LMN_evolv/reNormInputs.m
@@ -2,6 +2,10 @@ function input = reNormInputs(obj,normedInput,dimensions2renorm)
 %RENORMOUTPUT Summary of this function goes here
 %   Detailed explanation goes here
 
+if nargin == 2
+    dimensions2renorm = 1 : obj.dimIn;
+end
+
 input = normedInput;
 for i = 1 : length(dimensions2renorm) 
     input(i) = reNormData(normedInput(i),obj.inputRanges(i,:));
